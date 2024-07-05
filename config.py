6e4729f7dc006dfead21dd3c7c39bdb09e972354
flipflopphilosophy <sgarciadlt@gmail.com>
2024-07-05 16:12:37 -0400

    Your commit message here

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-# config.py
-
 import os
 from dotenv import load_dotenv
 
@@ -9,4 +7,3 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///job_candidate_manager.db')
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-    # Add other configurations as needed
